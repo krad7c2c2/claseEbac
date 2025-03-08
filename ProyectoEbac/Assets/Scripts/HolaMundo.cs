3dae630c1d74f830fe408575deb77dc24d97f0f7
Carlos Alberto Orta Cruz <caorta@telstock.net>
2025-03-07 20:50:59 -0600

    hola muno
    hollaaaa

---
+++ ProyectoEbac/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("esto solo es un print");
+        Debug.Log("Que onda");
+        Debug.LogError("Hola MUndo");
+        Debug.LogWarning("Algo va mal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
